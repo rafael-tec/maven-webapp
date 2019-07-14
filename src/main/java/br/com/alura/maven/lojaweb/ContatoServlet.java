7fa4d3004815679a5705e038fc4d250445e49b2a
rafael-tec <rafaeltecinfo9@gmail.com>
2019-07-14 20:23:27 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
@@ -0,0 +1,23 @@
+package br.com.alura.maven.lojaweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns={"/contato"})
+public class ContatoServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html><h2>Entre em contato</html></h2>");
+		writer.close();
+	}
+
+}
